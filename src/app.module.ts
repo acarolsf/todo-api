382e97985cb0acb9903b261512bc00962bf30e90
Ana Carolina <cscarol20@gmail.com>
2020-03-31 21:47:48 -0300

    Integrando e desenvolvendo o CRUD com o MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://acarolsf:<password>@cluster0-9rwan.gcp.mongodb.net/test?retryWrites=true&w=majority'),
+    // MongoClient.connect('mongodb+srv://user:<password>@cluster0-9rwan.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TaskService } from './shared/task.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TaskSchema } from './schemas/task.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: 'Task', schema: TaskSchema}])
+  ],
   controllers: [TasksController],
   providers: [TaskService],
 })
